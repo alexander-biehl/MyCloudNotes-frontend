58c888a99b2de0243d379041b6cacdd73a062d63
Alex Biehl <biehl.al@gmail.com>
2024-03-14 15:28:28 -0400

    added material components

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
-import './index.css'
-import './assets/styles/sakura.css'
+// import './index.css'
+// import './assets/styles/sakura.css'
+import './assets/styles/material-theme/css/theme.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
