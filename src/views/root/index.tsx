add84088d1984ecf73765b4eb4ec13a9a60299f4
Alex Biehl <biehl.al@gmail.com>
2024-03-23 16:57:36 -0400

    installed chakra and added an example sidebar

--- src/views/root/index.tsx
+++ src/views/root/index.tsx
@@ -1,31 +1,40 @@
-import { NavLink, Outlet /*useNavigation*/ } from 'react-router-dom'
+import { Outlet /*useNavigation*/ } from 'react-router-dom'
+import SimpleSideBar from '../../components/Sidebar'
 
 export default function Root() {
   // const navigation = useNavigation();
 
   return (
     <>
-      <div id="sidebar">
-        <h1>Menu</h1>
-        <nav>
-          <ul>
-            <li>
-              <NavLink to={`notes/`}>All Notes</NavLink>
-            </li>
-            <li>
-              <NavLink to={`categories/`}>Categories</NavLink>
-            </li>
-          </ul>
-        </nav>
-      </div>
-      {/* <div id="detail" className={
-                navigation.state === "loading" ? "loading" : ""
-            }>
-                <Outlet />
-            </div> */}
+      <SimpleSideBar />
       <div id="detail">
         <Outlet />
       </div>
     </>
   )
+  // return (
+  //   <>
+  //     <div id="sidebar">
+  //       <h1>Menu</h1>
+  //       <nav>
+  //         <ul>
+  //           <li>
+  //             <NavLink to={`notes/`}>All Notes</NavLink>
+  //           </li>
+  //           <li>
+  //             <NavLink to={`categories/`}>Categories</NavLink>
+  //           </li>
+  //         </ul>
+  //       </nav>
+  //     </div>
+  //     {/* <div id="detail" className={
+  //               navigation.state === "loading" ? "loading" : ""
+  //           }>
+  //               <Outlet />
+  //           </div> */}
+  //     <div id="detail">
+  //       <Outlet />
+  //     </div>
+  //   </>
+  // )
 }
