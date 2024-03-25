99d02eda5e822239d18825ce4472011713ae7397
Alex Biehl <biehl.al@gmail.com>
2024-03-25 17:32:43 -0400

    re-added Note detail view since I seem to have accidentally lost it

--- src/views/note/index.tsx
+++ src/views/note/index.tsx
@@ -1,17 +1,33 @@
 import { useLoaderData } from 'react-router-dom'
 import type { Note } from '../../types'
 import type { NoteResponse } from '../../network/api/types'
+import { Button, ButtonGroup, Card, CardBody, CardFooter, CardHeader, Heading } from '@chakra-ui/react'
 
 export default function Note() {
   const { note } = useLoaderData() as NoteResponse
 
   return (
     <>
-      <div id="note">
-        <div>Hello Noto!</div>
-        <h2>{note.title}</h2>
-        <p>{note.content ? note.content : 'No Content'}</p>
-      </div>
+      <Card size='lg' height='95vh'>
+        <CardHeader>
+          <Heading as='h1'>
+            {note.title}
+          </Heading>
+        </CardHeader>
+        <CardBody>
+          {note.content}
+        </CardBody>
+        <CardFooter>
+          <ButtonGroup>
+            <Button variant='solid' colorScheme='teal'>
+              Edit
+            </Button>
+            <Button variant='outline' colorScheme='red'>
+              Delete
+            </Button>
+          </ButtonGroup>
+        </CardFooter>
+      </Card>
     </>
   )
 }
