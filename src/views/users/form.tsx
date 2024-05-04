9afca426a34fc811369c37941e04e6219076d5b8
alexander-biehl <biehl.al@gmail.com>
2024-05-04 17:10:56 -0400

    started adding user-related action functions

---
+++ src/views/users/form.tsx
@@ -0,0 +1,53 @@
+import { Button, ButtonGroup, FormControl, FormLabel, Input } from '@chakra-ui/react'
+import { Form } from 'react-router-dom'
+
+/**
+ * @returns Form for registering new Users
+ */
+export const RegisterUser = () => {
+    return (
+        <>
+            <Form id="user-register" method="post" action="register">
+                <FormControl>
+                    <FormLabel htmlFor="username">User name</FormLabel>
+                    <Input id="username" name="username" placeholder="abc123" type="text" />
+                </FormControl>
+                <FormControl>
+                    <FormLabel htmlFor="password">Password</FormLabel>
+                    <Input id="password" name="password" type="password" />
+                </FormControl>
+                <FormControl>
+                    <FormLabel htmlFor="confirm_password">Confirm Password</FormLabel>
+                    <Input id="confirm_password" name="confirm_password" type="password" />
+                </FormControl>
+                <ButtonGroup>
+                    <Button variant={'solid'} colorScheme={'teal'} type={'submit'}>
+                        Submit
+                    </Button>
+                </ButtonGroup>
+            </Form>
+        </>
+    )
+}
+
+export const LoginUser = () => {
+    return (
+        <>
+            <Form id="user-register" method="post" action="register">
+                <FormControl>
+                    <FormLabel htmlFor="username">User name</FormLabel>
+                    <Input id="username" name="username" placeholder="abc123" type="text" />
+                </FormControl>
+                <FormControl>
+                    <FormLabel htmlFor="password">Password</FormLabel>
+                    <Input id="password" name="password" type="password" />
+                </FormControl>
+                <ButtonGroup>
+                    <Button variant={'solid'} colorScheme={'teal'} type={'submit'}>
+                        Submit
+                    </Button>
+                </ButtonGroup>
+            </Form>
+        </>
+    )
+}
